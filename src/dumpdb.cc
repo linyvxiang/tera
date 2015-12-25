acd3da6b896e8c14d2c9eaa13362783163a1fab3
yvxiang <linyvxiang@gmail.com>
2015-12-25 15:09:28 +0800

    Drop leveldb

--- src/dumpdb.cc
+++ src/dumpdb.cc
@@ -9,9 +9,11 @@
 #include "sdk/client_impl.h"
 #include "sdk/sdk_utils.h"
 
-#include "leveldb/db.h"
-#include "leveldb/write_batch.h"
 #include "proto/dumpdb.pb.h"
+
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 
 DECLARE_string(flagfile);
 DECLARE_string(tera_zk_addr_list);
@@ -66,14 +68,7 @@ int main(int argc, char* argv[])
             std::cout << "Open table: " << tablename << " error." << std::endl;
             return -1;
         }
-        leveldb::DB* db;
-        leveldb::Options options;
-        options.create_if_missing = true;
-        leveldb::Status s = leveldb::DB::Open(options, prefix, &db);
-        if (!s.ok()) {
-            std::cout << "Open dest db error" << std::endl;
-            return -1;
-        }
+
         std::string start_rowkey = "";
         std::string end_rowkey = "";
         ResultStream* result_stream;
@@ -86,6 +81,12 @@ int main(int argc, char* argv[])
             std::cout << "Scan src table error" << std::endl;
             return -1;
         }
+        std::fstream data_s(std::string(prefix + "data").c_str(),
+                std::ios::out | std::ios::trunc | std::ios::binary);
+        google::protobuf::io::ZeroCopyOutputStream *raw_out =
+            new google::protobuf::io::OstreamOutputStream(&data_s);
+        google::protobuf::io::CodedOutputStream *coded_out =
+            new google::protobuf::io::CodedOutputStream(raw_out);
         while (!result_stream->Done()) {
             /*
             std::cout << result_stream->RowName() << ":"
@@ -98,26 +99,29 @@ int main(int argc, char* argv[])
             if (columnfamily.find(":") != std::string::npos) {
                 columnfamily.resize(columnfamily.find(":"));
             }
+            record.set_rowname(result_stream->RowName());
             record.set_columnfamily(columnfamily);
             record.set_qualifier(result_stream->Qualifier());
             record.set_timestamp(result_stream->Timestamp());
             record.set_value(result_stream->Value());
             std::string infobuf;
             record.SerializeToString(&infobuf);
-            leveldb::Status s = db->Put(leveldb::WriteOptions(), result_stream->RowName(), infobuf);
-            assert(s.ok());
+            coded_out->WriteVarint32(infobuf.size());
+            coded_out->WriteRaw(infobuf.data(), infobuf.size());
             result_stream->Next();
         }
         std::cout << "dump table " << tablename << " successfully" << std::endl;
         delete table;
-        delete db;
+        delete coded_out;
+        delete raw_out;
+        data_s.close();
     } else if (strcmp(argv[1], "restore") == 0) {
         if (argc != 3) {
             Usage();
             return -1;
         }
-        std::string prefix = "./dump_data/" + tablename;
-        std::string schema_file = prefix + "/schema";
+        std::string prefix = "./dump_data/" + tablename + "/";
+        std::string schema_file = prefix + "schema";
         TableDescriptor table_desc;
         if (!ParseTableSchemaFile(schema_file, &table_desc)) {
             std::cout << "Parse schema error" << std::endl;
@@ -137,37 +141,44 @@ int main(int argc, char* argv[])
         }
 
         //restore records
-        leveldb::DB* db;
-        leveldb::Options options;
-        options.create_if_missing = false;
-        leveldb::Status s = leveldb::DB::Open(options, prefix, &db);
-        assert(s.ok());
-
         Table* table = NULL;
         if ((table = client->OpenTable(tablename, &error_code)) == NULL) {
             std::cout << "Open dest table error" << std::endl;
             return -1;
         }
+        std::fstream data_s(std::string(prefix + "data").c_str(),
+                std::ios::in | std::ios::binary);
+        if (!data_s) {
+            std::cout << "Open binary data error" << std::endl;
+            return -1;
+        }
+        google::protobuf::io::ZeroCopyInputStream *raw_in =
+            new google::protobuf::io::IstreamInputStream(&data_s);
+        google::protobuf::io::CodedInputStream *coded_in =
+            new google::protobuf::io::CodedInputStream(raw_in);
 
-        leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
-        for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        uint32_t message_size = 0;
+        while (coded_in->ReadVarint32(&message_size)) {
+            std::string message_buf;
+            coded_in->ReadString(&message_buf, message_size);
             tera::DumpRecord record;
-            record.ParseFromArray(it->value().data(), it->value().size());
-            std::string row_key(it->key().data(), it->key().size());
+            record.ParseFromString(message_buf);
             std::string columnfamily = "";
             std::string qualifier = "";
             if (record.has_columnfamily()) {
                 columnfamily = record.columnfamily();
                 qualifier = record.has_qualifier() ? record.qualifier() : "";
             }
-            if (!table->Put(row_key, columnfamily, qualifier, record.value(), &error_code)) {
+            if (!table->Put(record.rowname(), columnfamily, qualifier, record.value(), &error_code)) {
                 return -1;
             }
         }
+
         std::cout << "restore table " << tablename << " successfully" << std::endl;
-        delete it;
         delete table;
-        delete db;
+        delete coded_in;
+        delete raw_in;
+        data_s.close();
     } else {
         Usage();
         ret = -1;
